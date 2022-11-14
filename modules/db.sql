06dfa3fd27052121c65bac4db191dae8ed29e27a
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2022-11-13 22:51:50 -0300

    Actualizacion a db

--- modules/db.sql
+++ modules/db.sql
@@ -1,30 +1,30 @@
-CREATE DATABASE argentus;
+CREATE DATABASE argentus_working
 
-CREATE TABLE usuario (
+CREATE TABLE Clientes(
     userID int NOT NULL PRIMARY KEY,
     nombre varchar(255) NOT NULL,
     email varchar(255),
     telefono varchar(255) NOT NULL,
-    ciudad varchar(255) NOT NULL,
+    apellido varchar(255) NOT NULL,
     asesorID int FOREIGN KEY REFERENCES asesor(asesorID),
     planID int FOREIGN KEY REFERENCES planes(planID),
     pais varchar(255) FOREIGN KEY REFERENCES paises(nombre)
 );
 
-CREATE TABLE asesor (
+CREATE TABLE Asesor (
     asesorID int NOT NULL PRIMARY KEY, 
     nombre varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
     telefono varchar(255) NOT NULL,
 );
 
-CREATE TABLE planes (
+CREATE TABLE Planes (
     planID int NOT NULL PRIMARY KEY,
     costo int NOT NULL,
     isPremium boolean NOT NULL
 );
 
-CREATE TABLE paises (
+CREATE TABLE Paises (
     nombre varchar(255) NOT NULL PRIMARY KEY,
     requisitos varchar(255) NOT NULL,
     disponibilidad int
